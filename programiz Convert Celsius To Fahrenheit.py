f4dfd13581aa330389c9c0e6dedf0c0c0047d8d4
Monower Sadaf <sadafahmed720@gmail.com>
2022-12-05 20:46:28 +0600

    Python Program to Convert Celsius To Fahrenheit

---
+++ programiz Convert Celsius To Fahrenheit.py
@@ -0,0 +1,7 @@
+"""Python Program to Convert Celsius To Fahrenheit
+"""
+cel = float(input("enter the temperature in celcius: "));
+
+print("{} degree celsius to fahrenheit is: {}".format(cel, (cel*1.8)+32))
+
+
